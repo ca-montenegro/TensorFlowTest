d408aba5c73b1acaea18edeb8c7fb456c8898d1b
ca-montenegro <ca.montenegro@uniandes.edu.co>
2017-10-31 21:24:13 -0500

    modify

--- ComputationalGraph.py
+++ ComputationalGraph.py
@@ -13,7 +13,7 @@ a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
 adder_node = a + b #provides shortcut fot tf.add(a,b)
 print(sess.run(adder_node,{a:3, b:4.5}))
-print(sess.run(adder_node,{a:[1,3], b: [2,4]})) #matriz operatation
+print(sess.run(adder_node,{a:[1,3], b: [2,4]})) #matriz operation
 add_and_triple = adder_node*3
 print(sess.run(add_and_triple,{a:3, b:4.5}))
 
@@ -50,28 +50,28 @@ with tf.Session() as sess:
 print result
 
 
-x = tf.placeholder("float")
-y = tf.placeholder("float")
-# w is the variable storing our values. It is initialised with starting "guesses"
-# w[0] is the "a" in our equation, w[1] is the "b"
-w = tf.Variable([1.0, 2.0], name="w")
-# Our model of y = a*x + b
-y_model = tf.multiply(x, w[0]) + w[1]
-
-# Our error is defined as the square of the differences
-error = tf.square(y - y_model)
-# The Gradient Descent Optimizer does the heavy lifting
-train_op = tf.train.GradientDescentOptimizer(0.01).minimize(error)
-
-# Normal TensorFlow - initialize values, create a session and run the model
-model = tf.global_variables_initializer()
-
-with tf.Session() as session:
-    session.run(model)
-    for i in range(1000):
-        x_value = np.random.rand()
-        y_value = x_value * 2 + 6
-        session.run(train_op, feed_dict={x: x_value, y: y_value})
-
-    w_value = session.run(w)
-    print("Predicted model: {a:.3f}x + {b:.3f}".format(a=w_value[0], b=w_value[1]))
+# x = tf.placeholder("float")
+# y = tf.placeholder("float")
+# # w is the variable storing our values. It is initialised with starting "guesses"
+# # w[0] is the "a" in our equation, w[1] is the "b"
+# w = tf.Variable([1.0, 2.0], name="w")
+# # Our model of y = a*x + b
+# y_model = tf.multiply(x, w[0]) + w[1]
+#
+# # Our error is defined as the square of the differences
+# error = tf.square(y - y_model)
+# # The Gradient Descent Optimizer does the heavy lifting
+# train_op = tf.train.GradientDescentOptimizer(0.01).minimize(error)
+#
+# # Normal TensorFlow - initialize values, create a session and run the model
+# model = tf.global_variables_initializer()
+#
+# with tf.Session() as session:
+#     session.run(model)
+#     for i in range(1000):
+#         x_value = np.random.rand()
+#         y_value = x_value * 2 + 6
+#         session.run(train_op, feed_dict={x: x_value, y: y_value})
+#
+#     w_value = session.run(w)
+#     print("Predicted model: {a:.3f}x + {b:.3f}".format(a=w_value[0], b=w_value[1]))
